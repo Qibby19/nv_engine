5e5ed72ad20a084c97344446c5b1172901d6b58b
Qibby19 <133067147+Qibby19@users.noreply.github.com>
2025-04-24 21:03:10 +0800

    File picker
    -added file picker for scan

--- lib/history_database.dart
+++ lib/history_database.dart
@@ -2,24 +2,21 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class HistoryDatabase {
-  // all methods in this class are asynchronous because they involve I/O operations.
+  
   Future<Database> openMyDatabase() async {
     
 
     return await openDatabase(
-        // join method is used to join the path of the database with the path of the app's document directory.
+        
         join(await getDatabasesPath(), 'history1.db'),
-        // The version of the database. This is used to manage database schema changes.
+        
         version: 1,
-        // onCreate is a callback function that is called ONLY when the database is created for the first time.
+        
         onCreate: (db, version) {
       return db.execute(
         'CREATE TABLE history(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, amount  INTEGER, month INTEGER, day INTEGER, hour INTEGER, minute INTEGER)',
       );
-      //Here we are creating a table named todoList with three columns: id, title, and status.
-      //The id column is the primary key and is set to autoincrement.    
-      //We use INTEGER for the status column because SQLite does not have a boolean data type.
-      //Instead, we use 0 for false and 1 for true.    
+       
     });
   }
 
